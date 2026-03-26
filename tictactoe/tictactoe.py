c2d61caa116b4cc3ad377da4bd0c68e432563896
Diana Del Gaudio <49800866+dianadelg@users.noreply.github.com>
2026-03-26 17:16:06 -0400

    I actually brainstormed the algo

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,67 +1,85 @@
-# algorithm: create 3x3 grid with cols and rows number
-# print, player 1: take your turn. Ask them to enter as row, col 
-# When they enter, make sure spot not already taken. If so, say "spot unavailable"
-# otherwise, place element
-# players 1 and 2 take turns until no spots exist on the board
-# so it loops until all board spots are full (or 9 turns have happened) OR until someone has a vertical, 
-# horizontal, or diagonal match
+import sys
+import random
+
+choices = ['x', 'o']
+
+def generate_xo():
+    # to be used to switch up who goes first
+    return random.choice(choices)
 
 def printBoard(board):
-    print("\n   1   2   3")  # column headers
+    # to be used to print the board in a readable format
+    print("\n   1   2   3") 
     for i in range(3):
         print(str(i+1) + "  " + " | ".join(board[i]))
         if i < 2:
             print("  ---+---+---")
 
 
+def takeTurn(currentTurn, turnsLeft):
+    # called for each turn taken
+    print(currentTurn + "s turn")
+    row = int(input())
+    flag = True
+    
+    while flag == True:
+        row = -1
+        col = -1
+        while(row > 3 or row < 1):
+            print("Please select a valid row number: 1, 2, or 3:")
+            row = int(input())
+        while(col > 3 or col < 1):
+            print("Please select a valid col number: 1, 2, or 3:")
+            col = int(input())
+        if(board[row][col]!="_"):
+            flag = False
+            board[row][col] = currentTurn
+        else:
+            print("Spot already played, pick a new one")
+
+
+def didTurnWin(board, currentTurn):
+    #check to see if turn won
+    # can combine these but for now...
+    
+    # check horizontals
+    if (board[0][0] == currentTurn and board[0][1] == currentTurn and board [0][2] == currentTurn) or (board [1][0] == currentTurn and board[1][1] == currentTurn or board [1][2] == currentTurn) or (board [2][0] == currentTurn and board[2][1] == currentTurn and board[2][2] == currentTurn):
+        print(currentTurn + " has won!")
+        return True
+    # check verticals
+    elif (board[0][0] == currentTurn and board[1][0] == currentTurn and board [2][0] == currentTurn) or (board [0][1] == currentTurn and board[1][1] == currentTurn or board [2][1] == currentTurn) or (board [0][2] == currentTurn and board[1][2] == currentTurn and board[2][2] == currentTurn):
+        print(currentTurn + " has won!")
+        return True
+    # check diagonals
+    elif (board[0][0] == currentTurn and board[1][1] == currentTurn and board [2][2] == currentTurn) or (board [2][0] == currentTurn and board[1][1] == currentTurn or board [0][2] == currentTurn):
+        print(currentTurn + " has won!")
+        return True
+    else:
+        return False
+
+
 #Initialize board and print current board
 board = [["_","_","_"],["_","_","_"],["_","_","_"]]
 print("Welcome to Tic Tac Toe!")
-turnsLeft = 9
-hasWon = False
 
-while(turnsLeft > 1 and hasWon == False):
-    print("X's turn:")
-    row = -1
-    col = -1
-    while(row > 3 or row < 1 or row != "_"): ## meaning spot taken
-        #maybe error handle better?
-        print("Please select a valid row number: 1, 2, or 3:")
-        row = int(input())
-    while(col > 3 or col < 1):
-        print("Please select a valid col number: 1, 2, or 3:")
-        col = int(input())
-    if(board[row-1][col-1] == "_"):
-        print("Spot already played, pick new spots")
-        # need to add the logic to pick new spots. Maybe write algo better
-        while(row > 3 or row < 1 or row != "_"): ## meaning spot taken
-        #maybe error handle better?
-            print("Please select a valid row number: 1, 2, or 3:")
-            row = int(input())
-            while(col > 3 or col < 1):
-                print("Please select a valid col number: 1, 2, or 3:")
-                col = int(input())
-    board[row-1][col-1] = "X"
-    printBoard(board)
-    turnsLeft=turnsLeft-1
-    # need to check board for win -- tbd
+def main():
+    turnsLeft = 9
+    hasWon = False
+    currentTurn = (generate_xo())
+    while(turnsLeft > 1):
+        printBoard(board)
+        takeTurn(currentTurn, turnsLeft)
+        turnsLeft=turnsLeft-1
+        #check if turn caused a win
+        if(didTurnWin(board, currentTurn)):
+            printBoard(board) # print to show win
+            print("Game has been won by " + currentTurn)
+        else:
+            # switch to next player's turn based on current player
+            if choices[0] != currentTurn:
+                nextPlayer = choices[1]
+            else:
+                nextPlayer = choices[0]
 
-
-    print("O's turn:")
-    row = -1
-    col = -1
-    while(row > 3 or row < 1):
-        print("Please select a valid row number: 1, 2, or 3:")
-        row = int(input())
-    while(col > 3 or col < 1):
-        print("Please select a valid col number: 1, 2, or 3:")
-        col = int(input())
-        print("row :" + str(row) + " col: " + str(col))
-    board[row-1][col-1] = "O"
-    printBoard(board)
-    turnsLeft=turnsLeft-1
-
-
-
-
-
+if __name__ == "__main__":
+    sys.exit(main())
